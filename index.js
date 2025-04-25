ee72c0acc06f68bd5965608691d6288f9ac69f38
fatekkl <mathtml.1105@gmail.com>
2025-04-25 18:33:59 -0300

    remove unused packages + prepare commands

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const { getFuriaLastMatches } = require('./services/services');
-
-
 const bot_token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(bot_token, { polling: true });
 
+// Importar e usar os comandos
+const jogosCommand = require('./commands/jogos');
+const quizCommand = require('./commands/quiz');
+
+// Inicializar os comandos
+jogosCommand(bot);
+quizCommand(bot);
+
+// Comando /start
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "🐆 Bem-vindo ao FURIA Bot!\nUse /menu para ver o que posso fazer!");
 });
 
+// Comando /menu
 bot.onText(/\/menu/, (msg) => {
     bot.sendMessage(msg.chat.id,
         `
@@ -18,58 +25,4 @@ bot.onText(/\/menu/, (msg) => {
 /noticias - Últimas da FURIA
 /quiz - Teste seus conhecimentos
   `);
-});
-
-bot.onText(/\/jogos/, (msg) => {
-    const chatId = msg.chat.id
-
-    const options = {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: "Jogos Recentes", callback_data: "jogos_recentes" },
-                    { text: "Calendário Completo", callback_data: "calendario_completo" }
-                ]
-            ]
-        }
-    }
-
-    bot.sendMessage(chatId, "🎮 O que você quer saber?", options);
-})
-
-
-bot.on("callback_query", async (callbackQuery) => {
-    const chatId = callbackQuery.message.chat.id
-    const data = callbackQuery.data
-    const matches = await getFuriaLastMatches()
-
-
-    switch (data) {
-        case "jogos_recentes":
-            let recentMessage = '';
-
-            for (let i = 0; i < 5; i++) {
-                recentMessage += `🏆 Jogo ${i + 1}:\n`;
-                recentMessage += `${matches[i].team1} vs ${matches[i].team2}\n`;
-                recentMessage += `Placar: ${matches[i].score}\n`;
-                recentMessage += `Evento: ${matches[i].event}\n\n`;
-            }
-
-            bot.sendMessage(chatId, recentMessage)
-            break;
-        case "calendario_completo":
-            let completeMessage = ''
-
-            for (let i = 0; i < 25; i++) {
-                completeMessage += `🏆 Jogo ${i + 1}:\n`;
-                completeMessage += `${matches[i].team1} vs ${matches[i].team2}\n`;
-                completeMessage += `Placar: ${matches[i].score}\n`;
-                completeMessage += `Evento: ${matches[i].event}\n\n`;
-            }
-            bot.sendMessage(chatId, completeMessage)
-            break;
-
-        default:
-            break;
-    }
 });
