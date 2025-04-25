a6f2d28be12b7c930effbb6b218b966d4c9d32a6
fatekkl <mathtml.1105@gmail.com>
2025-04-25 18:33:47 -0300

    change commands responsability

---
+++ commands/jogos.js
@@ -0,0 +1,56 @@
+// commands/jogos.js
+const { getFuriaLastMatches } = require('../services/services');
+
+module.exports = (bot) => {
+    bot.onText(/\/jogos/, async (msg) => {
+        const chatId = msg.chat.id;
+
+        const options = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "Jogos Recentes", callback_data: "jogos_recentes" },
+                        { text: "Últimos 25 jogos", callback_data: "ultimos_25" }
+                    ]
+                ]
+            }
+        };
+
+        bot.sendMessage(chatId, "🎮 O que você quer saber?", options);
+    });
+
+    bot.on("callback_query", async (callbackQuery) => {
+        const chatId = callbackQuery.message.chat.id;
+        const data = callbackQuery.data;
+        const matches = await getFuriaLastMatches();
+
+        switch (data) {
+            case "jogos_recentes":
+                let recentMessage = '';
+
+                for (let i = 0; i < 5; i++) {
+                    recentMessage += `🏆 Jogo ${i + 1}:\n`;
+                    recentMessage += `${matches[i].team1} vs ${matches[i].team2}\n`;
+                    recentMessage += `Placar: ${matches[i].score}\n`;
+                    recentMessage += `Evento: ${matches[i].event}\n\n`;
+                }
+
+                bot.sendMessage(chatId, recentMessage);
+                break;
+            case "ultimos_25":
+                let completeMessage = '';
+
+                for (let i = 0; i < 25; i++) {
+                    completeMessage += `🏆 Jogo ${i + 1}:\n`;
+                    completeMessage += `${matches[i].team1} vs ${matches[i].team2}\n`;
+                    completeMessage += `Placar: ${matches[i].score}\n`;
+                    completeMessage += `Evento: ${matches[i].event}\n\n`;
+                }
+                bot.sendMessage(chatId, completeMessage);
+                break;
+
+            default:
+                break;
+        }
+    });
+};
---
+++ commands/quiz.js
@@ -0,0 +1,80 @@
+// commands/quiz.js
+module.exports = (bot) => {
+    bot.onText(/\/quiz/, (msg) => {
+        const chatId = msg.chat.id;
+
+        const questions = [
+            {
+                question: "Qual foi o primeiro título internacional conquistado pela FURIA no CS:GO?",
+                options: ["DreamHack Masters Las Vegas 2017", "ESL One Cologne 2019", "IEM Katowice 2020", "StarLadder Major Berlin 2019"],
+                answer: "DreamHack Masters Las Vegas 2017"
+            },
+            {
+                question: "Quem é o jogador da FURIA conhecido por ser um dos melhores AWPer do mundo?",
+                options: ["yuurih", "kNgV-", "art", "VINI"],
+                answer: "yuurih"
+            },
+            {
+                question: "Em que torneio a FURIA obteve sua primeira vitória em um evento Major?",
+                options: ["IEM Katowice 2019", "StarLadder Major Berlin 2019", "ESL Pro League Season 11", "IEM Beijing 2020"],
+                answer: "StarLadder Major Berlin 2019"
+            },
+            {
+                question: "Qual é a composição atual da FURIA CS:GO (incluindo o capitão)?",
+                options: ["art, yuurih, VINI, kNgV-, drop", "art, yuurih, VINI, KSCERATO, HEN1", "art, yuurih, VINI, KSCERATO, drop", "art, KSCERATO, VINI, yuurih, HEN1"],
+                answer: "art, yuurih, VINI, KSCERATO, drop"
+            },
+            {
+                question: "Qual jogador da FURIA foi eleito o MVP da ECS Season 8 Finals?",
+                options: ["art", "yuurih", "kNgV-", "VINI"],
+                answer: "yuurih"
+            }
+        ];
+
+        let currentQuestionIndex = 0;
+        let score = 0;
+
+        const askQuestion = () => {
+            const currentQuestion = questions[currentQuestionIndex];
+            const options = currentQuestion.options.map(option => ({
+                text: option,
+                callback_data: option
+            }));
+
+            const questionText = currentQuestion.question;
+
+            const optionsMarkup = {
+                reply_markup: {
+                    inline_keyboard: options.map(option => [option])
+                }
+            };
+
+            bot.sendMessage(chatId, questionText, optionsMarkup);
+        };
+
+        const checkAnswer = (callbackQuery) => {
+            const userAnswer = callbackQuery.data;
+            const correctAnswer = questions[currentQuestionIndex].answer;
+
+            if (userAnswer === correctAnswer) {
+                score++;
+                bot.sendMessage(callbackQuery.message.chat.id, "✅ Resposta correta!");
+            } else {
+                bot.sendMessage(callbackQuery.message.chat.id, "❌ Resposta errada!");
+            }
+
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.length) {
+                askQuestion();
+            } else {
+                bot.sendMessage(callbackQuery.message.chat.id, `🎉 Você completou o quiz! Sua pontuação final é: ${score} de ${questions.length}.`);
+            }
+        };
+
+        askQuestion();
+
+        bot.on("callback_query", (callbackQuery) => {
+            checkAnswer(callbackQuery);
+        });
+    });
+};
